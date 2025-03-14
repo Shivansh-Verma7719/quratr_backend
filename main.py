cd39e18417784be43b0081c6c48a00115975b570
Shivansh Verma <shivanshverma082004@gmail.com>
2025-03-14 21:21:15 +0530

    remove cors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
 from v1.routers.places import router as places_router
 
 
@@ -8,18 +8,17 @@ app = FastAPI(
     description="API for Quratr",
 )
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-]
+# origins = [
+#     "**",
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 @app.get("/")
 def read_root():
