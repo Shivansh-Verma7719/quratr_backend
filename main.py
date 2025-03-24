3a6253eb13354b1641b3ec9421b02730efe27614
Shivansh Verma <shivanshverma082004@gmail.com>
2025-03-25 02:25:48 +0530

    fix added cors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from v1.routers.places import router as places_router
 
 app = FastAPI(
@@ -7,17 +7,17 @@ app = FastAPI(
     description="API for Quratr AI",
 )
 
-# origins = [
-#     "**",
-# ]
+origins = [
+    "**",
+]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
